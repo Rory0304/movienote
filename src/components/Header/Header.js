72a1cb3910a94094a5b210ae45e608d8c5fcd8f4
Sa Eun Soo <tkdmstn0304@naver.com>
2020-11-11 20:20:35 +0900

    add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,43 +1,58 @@
-import React from "react";
+import React  from "react";
 import { Link } from "react-router-dom";
 import {FaList} from "react-icons/fa";
 
-function Header() {
-  return (
+class Header extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.menu = React.createRef();
+  }
+
+  showMenu = () => {
+    if(this.menu.current.style.display === "block"){
+      this.menu.current.style.display = "none";
+    }
+    else{
+      this.menu.current.style.display = "block";
+    }
+  }
+
+  render(){
+    return(
     <div className="header">
-      <FaList id="header_log" />
-      <input className="seanprchbar" />
-      <Link to="/home">
-        <h1>Movie Note</h1>
+      <FaList id="header_menu" onClick={this.showMenu}/>
+      <nav className="submenu">
+       <ul  ref={this.menu}>
+         <Link to="/>" style={{ textDecoration: 'none' }}>
+           <li>Now Playing</li>
+          </Link>
+          <Link to="/upcoming" style={{ textDecoration: 'none' }}>
+           <li>Upcoming</li>
+          </Link>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+           <li>Popular</li>
+          </Link>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+           <li>Top Rated</li>
+          </Link>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+           <li>My Note</li>
+          </Link>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+           <li>Article</li>
+          </Link>
+        </ul>
+      </nav>
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <h1>Movie Note.</h1>
       </Link>
       <div className="userinfo">
         <button>Login</button>
         <button>Sign up</button>
       </div>
-      <nav className="submenu">
-        <ul>
-          <Link to="/>">
-            <li>Now Playing</li>
-          </Link>
-          <Link to="/upcoming">
-            <li>Upcoming</li>
-          </Link>
-          <Link to="/">
-            <li>Popular</li>
-          </Link>
-          <Link to="/">
-            <li>Top Rated</li>
-          </Link>
-          <Link to="/">
-            <li>My Note</li>
-          </Link>
-          <Link to="/">
-            <li>Article</li>
-          </Link>
-        </ul>
-      </nav>
     </div>
-  );
+ );}
 }
 
 export default Header;
