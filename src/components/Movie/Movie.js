a0f5de75823ffa18bc2ed6c28eda45b6b6f0068a
Sa Eun Soo <tkdmstn0304@naver.com>
2020-11-11 20:21:36 +0900

    add movie detail info page

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,58 +1,78 @@
-import React, { useState, useRef } from "react";
-// import { render } from "@testing-library/react";
+import React  from "react";
 import Review from "../Review/Review";
 import Axios from "axios";
 import "./Movie.css";
+import {commonAPI, secretKey} from "../../variable";
 import MovieReviewSample from "./MovieReviewSample.js";
+
 
 class Movie extends React.Component {
   constructor(props) {
     super(props);
-
+    
     this.state = {
-      id: props.location.state.id,
-      poster: props.location.state.poster,
-      title: props.location.state.title,
-      date: props.location.state.date,
-      overview: props.location.state.overview,
+      id: this.props.location.state.id,
+      poster: this.props.location.state.poster,
+      title: this.props.location.state.title,
+      date: this.props.location.state.date,
+      overview: this.props.location.state.overview,
+      backdrop : this.props.location.state.backdrop,
+      popularity: this.props.location.popularity,
       casts: [],
-      value: ''
+      review: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    this.getMovieCasts = this.getMovieCasts.bind(this);
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+    this.reviewSection = React.createRef();
   }
-  handleSubmit(event) {
- }
 
   componentDidMount() {
     this.getMovieCasts();
+    window.scrollTo(0, 0);
   }
 
-  getMovieCasts = async (id = this.state.id) => {
+  /* review section*/
+  handleChange(event) {
+    this.setState({review: event.target.value});
+  }
 
+  handleSubmit(event) {
+
+  }
+
+  openCloseReview = () => {
+    console.log("here");
+    if(this.reviewSection.current.style.display === "block"){
+      this.reviewSection.current.style.display = "none";
+    }
+    else{
+      this.reviewSection.current.style.display = "block";
+    }
+  }
+
+  /* get movie casts list*/
+  getMovieCasts = async() => {
     try{
-      const cast_url = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=b1306395631dc84cde154096963c13db`;
-      await Axios.get(cast_url).then(res => this.setState({casts : res.cast}));
+      const cast_url = `${commonAPI}${this.state.id}/credits?api_key=${secretKey}`;
+      await Axios.get(cast_url).then(res => this.setState({casts : res.data.cast}));
     }catch(e){
       return e;
     }
   };
 
   render() {
-    const { id, poster, title, date, overview, casts } = this.state;
-    const posterSrc =
-      poster === null ? null : "http://image.tmdb.org/t/p/w185" + poster;
-
-    function showReview() {}
-
+    const { id, poster, title, date, overview, casts, backdrop, popularity } = this.state;
+    const posterSrc = poster === null ? null : "http://image.tmdb.org/t/p/w185" + poster;
+    const backdropSrc = backdrop === null? null : "https://image.tmdb.org/t/p/original" + backdrop;
+    
+    console.log(popularity);
     return (
-      <div>
+      <div ref={this.wrapper}>
         <div className="movie_detail_header">
+          <div className="header_backdrop" style={{ backgroundImage : `url(${backdropSrc})`}}></div>
           {poster === null ? (
             <div className="header_noposter poster"></div>
           ) : (
@@ -61,20 +81,20 @@ class Movie extends React.Component {
           <div className="header_info">
             <h2>{title}</h2>
             <p>{date}</p>
-
+            <p>{popularity}</p>
             <button>Movie Note</button>
-            <button onClick="showReview()">Review</button>
+            <button onClick={this.openCloseReview}>Review</button>
           </div>
         </div>
 
-        <form className="reviewPage" onSubmit={this.handleSubmit}>
+        <form className="reviewPage" onSubmit={this.handleSubmit} ref={this.reviewSection}>
           <header>
-            <div className="reviewCancel">취소</div>
+            <div className="reviewCancel" onClick={this.openCloseReview}>취소</div>
             <input type="submit" value="저장" class="reviewSave" />
             <em> {title}</em>
           </header>
           <div className="reviewTextarea">
-            <textarea value={this.state.value} onChange={this.handleChange} placeholder="리뷰 작성"></textarea>
+            <textarea value={this.state.review} onChange={this.handleChange} placeholder="리뷰 작성"></textarea>
           </div>
         </form>
 
@@ -85,7 +105,7 @@ class Movie extends React.Component {
           </section>
 
           <section className="Top Billed Cast">
-            <h3 className="section_header">Top_Billed_Cast</h3>
+            <h3 className="section_header">Top Billed Cast</h3>
             <div className="casts">
               {casts.map((cast) => {
                 return (
@@ -112,6 +132,7 @@ class Movie extends React.Component {
   }
 }
 
+/* componenet of movie cast info section*/
 class CastInfo extends React.Component {
   render() {
     const { character, name, profile_path } = this.props;
