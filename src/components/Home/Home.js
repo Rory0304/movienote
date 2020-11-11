84fe058dffebe46821286370dadc16d21abb5ccc
Sa Eun Soo <tkdmstn0304@naver.com>
2020-11-11 20:21:08 +0900

    add main home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,14 +13,18 @@ class Home extends React.Component {
       upComingList : [],
       nowPlayingList : [],
       upComingCurrent : 0,
+      nowPlayingCurrent : 0,
     }
 
     this.getPopularList = this.getPopularList.bind(this);
     this.getupComingList = this.getupComingList.bind(this);
     this.getNowplayingList = this.getNowplayingList.bind(this);
-
+    
     this.totalSlides = 5;
+    this.nowplayingTotalSlides = 3;
     this.upcomingCarousel = React.createRef();
+    this.nowplayingCarousel = React.createRef();
+
   }
 
   componentDidMount(){
@@ -45,7 +49,7 @@ class Home extends React.Component {
     try{
       const nowPlaying_url = `${commonAPI}now_playing?api_key=${secretKey}&language=en-US&page=1`;
       await Axios.get(nowPlaying_url)
-      .then(res => this.setState({nowPlayingList : res.data.results.slice(0,5)}))
+      .then(res => this.setState({nowPlayingList : res.data.results.slice(0,12)}))
       .catch(e => console.log(e));
     }
     catch(e){
@@ -65,22 +69,63 @@ class Home extends React.Component {
   };
 
 
-  moveSlide = () => {
-    const size = this.upcomingCarousel.current.clientWidth;
-    this.upcomingCarousel.current.style.transition = "transform 0.4s ease-in-out";
-    this.upcomingCarousel.current.style.transform = "translateX(" + (-size * this.state.upComingCurrent) + "px)";
+  moveSlide = (props) => {
+
+
+    if(props === "upcoming"){
+      const size = this.upcomingCarousel.current.clientWidth;
+      this.upcomingCarousel.current.style.transition = "transform 0.4s ease-in-out";
+      this.upcomingCarousel.current.style.transform = "translateX(" + (-size * this.state.upComingCurrent) + "px)";
+    }
+    else{
+      const size = this.nowplayingCarousel.current.clientWidth;
+      this.nowplayingCarousel.current.style.transition = "transform 0.4s ease-in-out";
+      this.nowplayingCarousel.current.style.transform = "translateX(" + (-size * this.state.nowPlayingCurrent) + "px)";
+    }
+    
+  }
+
+
+  clickPrevBtn_nowPlaying = (e) => {
+    e.preventDefault();
+
+    if(this.state.nowPlayingCurrent === 0){
+      this.setState({nowPlayingCurrent : this.nowplayingTotalSlides-1}, function(){
+        this.moveSlide("nowplaying");
+      })
+    }
+    else{
+      this.setState({nowPlayingCurrent : this.state.nowPlayingCurrent -1}, function(){
+        this.moveSlide("nowplaying");
+      })
+    }
+  }
+
+  clickNextBtn_nowPlaying = (e) => {
+    e.preventDefault();
+    if(this.state.nowPlayingCurrent === this.nowplayingTotalSlides -1){
+      this.setState({nowPlayingCurrent : 0}, function(){
+        this.moveSlide("nowplaying");
+      });
+    }
+    else{
+      this.setState({nowPlayingCurrent : this.state.nowPlayingCurrent + 1},function(){
+        this.moveSlide("nowplaying");
+      });
+    }
   }
 
   clickPrevBtn = (e) => {
     e.preventDefault();
+
      if(this.state.upComingCurrent === 0){
        this.setState({upComingCurrent : this.totalSlides -1}, function(){
-         this.moveSlide();
+         this.moveSlide("upcoming");
        });
      }
      else{
        this.setState({upComingCurrent : this.state.upComingCurrent -1}, function(){
-         this.moveSlide();
+         this.moveSlide("upcoming");
        });
      }
   }
@@ -90,12 +135,12 @@ class Home extends React.Component {
     e.preventDefault();
      if(this.state.upComingCurrent === this.totalSlides -1){
        this.setState({upComingCurrent : 0}, function(){
-         this.moveSlide();
+         this.moveSlide("upcoming");
        });
      }
      else{
        this.setState({upComingCurrent : this.state.upComingCurrent + 1},function(){
-         this.moveSlide();
+         this.moveSlide("upcoming");
        });
      }
   }
@@ -112,17 +157,24 @@ class Home extends React.Component {
       </div>
       <div className="nowplaying_area">
         <h2>Now Playing</h2>
-        {nowPlayingList.map((list) => <NowPlayingArea list={list} key={list.id} />)}
+        <FaChevronLeft className="arrow nowplaying_left" onClick={this.clickPrevBtn_nowPlaying}/>
+        <div className="nowplaying_movies">
+          <div className="nowplaying_movies_carousel"  ref={this.nowplayingCarousel}>
+          {nowPlayingList.map((list) => <NowPlayingArea list={list} key={list.id}/>)}
+          </div>
+        </div>
+        <FaChevronRight className="arrow nowplaying_right" onClick={this.clickNextBtn_nowPlaying}/>
+        
       </div>
       <div className="upcoming_area">
         <h2>Upcoming</h2>
-        <FaChevronLeft className="arrow left" onClick={this.clickPrevBtn}/>
+        <FaChevronLeft className="arrow upcoming_left" onClick={this.clickPrevBtn}/>
         <div className="upcoming_movies">
           <div className="upcoming_movies_carousel" ref={this.upcomingCarousel}>
           {upComingList.map((list, index) => <UpcomingArea list={list} key={list.id} index={index}/>)}
           </div>
         </div>
-        <FaChevronRight className="arrow right" onClick={this.clickNextBtn} />
+        <FaChevronRight className="arrow upcoming_right" onClick={this.clickNextBtn} />
       </div>
     </div>
     );
@@ -144,12 +196,13 @@ function PopularArea({list}){
 }
 
 function NowPlayingArea({list}){
-  const poster_path = "http://image.tmdb.org/t/p/w185" + list.poster_path;
+  const poster_path = `url(http://image.tmdb.org/t/p/w185${list.poster_path})`;
   return(
-  <div className="nowPlaying_movie">
-    <img alt={list.title} src={poster_path}/>
-    <h3>{list.title}</h3>
-    <p>{list.release_date}</p>
+  <div className="nowplaying_movie" style={{ backgroundImage: poster_path }} >
+    <section className="nowplaying_movie_info">
+      <h3>{list.title}</h3>
+      <p>{list.release_date}</p> 
+    </section>
   </div>
   )
 }
@@ -164,7 +217,6 @@ class UpcomingArea extends React.Component{
 
     return(
     <div className="upcoming_movie">
-      {index}
       <img alt={list.title} src={poster_path}/>
       <section><h3>{list.title}</h3>
       <p>{list.release_date}</p>
