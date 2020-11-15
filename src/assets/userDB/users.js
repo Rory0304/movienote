6a746a664ff7ecfe46193274de537ed4d08c88f1
Sa Eun Soo <tkdmstn0304@naver.com>
2020-11-16 01:06:03 +0900

    add user database

---
+++ src/assets/userDB/users.js
@@ -0,0 +1,18 @@
+module.exports = {
+    users : [
+    {
+        id: "rory",
+        pw : "0304",
+        nickname : "로리"
+    },
+    {
+        id : "park",
+        pw : "1111",
+        nickname : "park"
+    },
+    {
+        id : "moon",
+        pw : "2222",
+        nickname : "moon"
+    }]
+}
